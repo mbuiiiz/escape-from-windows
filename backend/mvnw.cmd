092eace362466e78cf0b0557fcc0068792e37566
James Hoang <qmh1@sfu.ca>
2026-01-31 20:33:36 -0800

    removed backend/

--- backend/mvnw.cmd
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-@REM TODO: add Maven Wrapper script (optional)
-
-=======
-@echo off
-rem Maven Wrapper Script for Windows
-mvn %*
->>>>>>> backend/rest
--- backend/src/main/java/com/hackathon/xpgame/repo/SessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hackathon.xpgame.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hackathon.xpgame.model.PlayerSession;
-
-public interface SessionRepository extends JpaRepository<PlayerSession, String> {
-    
-}
