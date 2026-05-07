af8dfad5a1ba0480b32b8f85da368e8a29145377
mbuiiiz <ductai2068@gmail.com>
2026-05-06 22:49:18 -0700

    Add unit tests for XpGameApplication and HealthController

---
+++ backend/src/test/java/com/hackathon/xpgame/XpGameApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hackathon.xpgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
